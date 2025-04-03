64b5d6637650e09a26eaab0ce690135ba94f2284
DaviMoreira27 <davimsantana2706@gmail.com>
2025-04-03 20:18:57 -0300

    wip(exec02): added positive check and create list method

---
+++ FunctionalProgramming/exec02/exec.hs
@@ -0,0 +1,15 @@
+main :: IO ()
+main = do
+    firstV <- getLine
+    secondV <- getLine
+    let a = read firstV
+    let b = read secondV
+    putStrLn $ show $ createList a b
+
+isPositive :: (Integral a) => a -> a -> Bool
+isPositive x y = x > 0 && y > 0
+
+createList a b
+    | isPositive a b == True = [a.. b]
+    | otherwise = []
+    
