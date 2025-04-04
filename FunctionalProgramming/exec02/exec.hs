e0e510ba90b21ad651abfddbb0692ddd28743731
DaviMoreira27 <davimsantana2706@gmail.com>
2025-04-03 21:06:50 -0300

    wip(exec02): added getDivisers and and variables

--- FunctionalProgramming/exec02/exec.hs
+++ FunctionalProgramming/exec02/exec.hs
@@ -6,6 +6,10 @@ main = do
     let b = read secondV
     putStrLn $ show $ createList a b
 
+perfectNumber = 0
+abudantNumber = 0
+defectNumber = 0
+
 isPositive :: (Integral a) => a -> a -> Bool
 isPositive x y = x > 0 && y > 0
 
@@ -13,4 +17,17 @@ createList :: (Integral a) => a -> a -> [a]
 createList a b
     | isPositive a b == True = [a.. b]
     | otherwise = []
+
+-- For each item in this list I must get all their dividers. Then I sum all the dividers
+-- And compare to see if its smaller, bigger or equal than the number
     
+getDividers :: Int -> [Int]
+getDividers n = filter (\x -> n `mod` x == 0) [1..n]
+
+getResult :: [Int] -> ()
+getResult [] = 0
+getResult (x:xs)
+    | sum (getDividers x) + getResult xs == x = perfectNumber +=
+    | sum (getDividers x) + getResult xs > x = abudantNumber +=
+    | sum (getDividers x) + getResult xs < x = defectNumber +=
+
