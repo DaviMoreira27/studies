b3db34551d6eddf16cfca1ee2769d7b8c38d41b9
DaviMoreira27 <davimsantana2706@gmail.com>
2025-12-07 21:57:36 -0300

    refactor: removed unecessary test cases

--- testing-software-validation/mutations/mutation.py
+++ testing-software-validation/mutations/mutation.py
@@ -8,35 +8,6 @@ class TestPlacarMutation(unittest.TestCase):
         self.placar = Placar()
         self.placar.__init__()
 
-    def test_placar_inicia_com_zero(self):
-        esperado = [0] * 10
-        self.assertEqual(self.placar.placar, esperado)
-
-    def test_taken_inicia_com_false(self):
-        esperado = [False] * 10
-        self.assertEqual(self.placar.taken, esperado)
-
-    def test_nomes_tamanho(self):
-        esperado = 10
-        self.assertEqual(len(self.placar.nomes), esperado)
-
-    def test_nomes_conteudo_e_ordem(self):
-            p = Placar()
-            esperado = [
-                "Ones",
-                "Twos",
-                "Threes",
-                "Fours",
-                "Fives",
-                "Sixes",
-                "Full",
-                "Sequence",
-                "Four of a kind",
-                "General",
-            ]
-
-            self.assertEqual(p.nomes, esperado)
-
     def test_str_formato_colunas(self):
             p = Placar()
             resultado = str(p)
--- testing-software-validation/mutations/testa_placar.py
+++ testing-software-validation/mutations/testa_placar.py
@@ -1,5 +1,9 @@
 import os
 
+# Lucas Michael Genovese Huss Oliveira - 15577610
+# Davi Moreira de Santana - 15447584
+# Larissa de Melo Andrade - 15415640
+
 # Cria a sessão de teste
 os.system("python -m pyproteum testnew --D . --S placar.py placar_session")
 
